8cfb9945b6c38fe76b3f7c7bff4712e254841ea8
Hrishi Mittal <hrishimittal@gmail.com>
2017-08-17 16:25:45 +0100

    Add create and update endpoints

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,8 +1,25 @@
 module Api::V1
 	class IdeasController < ApplicationController
 	  def index
-	  	@ideas = Idea.all
+      @ideas = Idea.order("created_at DESC")
 	  	render json: @ideas
 	  end
+
+    def create
+      @idea = Idea.create(idea_params)
+      render json: @idea
+    end
+
+    def update
+      @idea = Idea.find(params[:id])
+      @idea.update_attributes(idea_params)
+      render json: @idea
+    end
+
+    private
+
+    def idea_params
+      params.require(:idea).permit(:title, :body)
+    end
 	end
 end
--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,12 @@ module IdeaboardApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
   end
 end
